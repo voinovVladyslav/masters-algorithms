68e58cbc9af1e3cc922f62a6cc5d0953bd1cf298
Voinov Vladyslav <voinovvladyslav@gmail.com>
2025-02-18 19:56:00 +0200

    refactor bellman ford to use adjacency matrix

--- graphs/bellman_ford.py
+++ graphs/bellman_ford.py
@@ -1,6 +1,13 @@
 import math
 
-from graphs.types import CostsDict, GraphAsDict, ParentsDict
+from graphs.types import (
+    AdjacencyMatrix,
+    Cost,
+    CostsDict,
+    GraphAsDict,
+    Node,
+    ParentsDict,
+)
 
 
 def initialize_single_source(
@@ -17,17 +24,18 @@ def initialize_single_source(
     return costs, parents
 
 
+def produce_result(parents: ParentsDict, end: str) -> tuple[str, ...]:
+    result = [end]
+    parent = parents.get(end, None)
+    while parent:
+        result.append(parent)
+        parent = parents.get(parent, None)
+    return tuple(result)
+
+
 def bellman_ford(
     graph: GraphAsDict, start: str, end: str
 ) -> tuple[bool, tuple[str, ...] | None]:
-    def produce_result(parents: ParentsDict, end: str) -> tuple[str, ...]:
-        result = [end]
-        parent = parents.get(end, None)
-        while parent:
-            result.append(parent)
-            parent = parents.get(parent, None)
-        return tuple(result)
-
     costs, parents = initialize_single_source(graph, start)
     for _ in graph.items():
         for from_vert, vertice in graph.items():
@@ -42,4 +50,47 @@ def bellman_ford(
             from_to_cost = graph[from_vert][to_vert]
             if costs[to_vert] > costs[from_vert] + from_to_cost:
                 return False, None
+
     return True, produce_result(parents, end)
+
+
+def produce_result_new(parents: list[Node | None], end: int) -> tuple[int, ...]:
+    result = [end]
+    parent = parents[end]
+    while parent is not None:
+        result.append(parent)
+        parent = parents[parent]
+    return tuple(result)
+
+def initialize_single_source_new(
+    graph: AdjacencyMatrix, start: int
+) -> tuple[list[Cost], list[Node | None]]:
+    parents: list[Node | None] = [None] * len(graph)
+    costs: list[Cost] = [math.inf] * len(graph)
+    costs[start] = 0
+    return costs, parents
+
+
+def bellman_ford_new(
+    graph: AdjacencyMatrix, start: int, end: int
+) -> tuple[bool, tuple[int, ...] | None]:
+    costs, parents = initialize_single_source_new(graph, start)
+    for _ in range(len(graph)):
+        for from_vert, vertice in enumerate(graph):
+            for to_vert, cost in enumerate(vertice):
+                if cost is None:
+                    continue
+
+                if costs[to_vert] > costs[from_vert] + cost:
+                    costs[to_vert] = costs[from_vert] + cost
+                    parents[to_vert] = from_vert
+
+    for from_vert, vertice in enumerate(graph):
+        for to_vert, cost in enumerate(vertice):
+            if cost is None:
+                continue
+
+            if costs[to_vert] > costs[from_vert] + cost:
+                return False, None
+
+    return True, produce_result_new(parents, end)
--- main.py
+++ main.py
@@ -1,38 +1,42 @@
-from graphs.dijkstra import dijkstra
+from graphs.dijkstra import dijkstra  # noqa
+from graphs.bellman_ford import bellman_ford, bellman_ford_new
+from graphs.utils import graph_to_dict
+
 from graphs.types import AdjacencyMatrix
 
 _ = None
 # Dijkstra
+# graph: AdjacencyMatrix = [
+#     # a  b  c  d  e  f
+#     [_, 1, 5, 10, _, _],  # a
+#     [_, _, 13, _, _, _],  # b
+#     [_, _, _, _, 2, _],  # c
+#     [_, _, 1, _, _, 12],  # d
+#     [_, _, _, _, _, 5],  # e
+#     [_, _, _, _, _, _],  # f
+# ]
+
+# Bellman Ford
 graph: AdjacencyMatrix = [
-    # a  b  c  d  e  f
-    [_, 1, _, 10, _, _],  # a
-    [_, _, 13, _, _, _],  # b
-    [_, _, _, _, 2, _],  # c
-    [_, _, 1, _, _, 12],  # d
-    [_, _, _, _, _, 5],  # e
+    # a b  c  d  e  f
+    [_, 5, 2, _, _, _],  # a
+    [1, _, _, _, 3, _],  # b
+    [_, -1, _, -5, 3, _],  # c
+    [_, _, _, _, _, 0],  # d
+    [_, _, 2, 2, _, 9],  # e
     [_, _, _, _, _, _],  # f
 ]
-
-# Bellman Ford
-# graph = [
+# Bellman Ford, negative cycle
+# graph: AdjacencyMatrix = [
 #     # a b  c  d  e  f
 #     [_, 5, 2, _, _, _],  # a
-#     [1, _, _, _, 3, _],  # b
+#     [1, _, _, _, -3, _],  # b
 #     [_, -1, _, -5, 3, _],  # c
-#     [_, _, _, _, _, 0],  # d
-#     [_, _, 2, 2, _, 9],  # e
-#     [_, _, _, _, _, _],  # f
-# ]
-# Bellman Ford
-# graph = [
-#     # a b  c  d  e  f
-#     [_, 5, 2, _, _, _],  # a
-#     [1, _, _, _, 3, _],  # b
-#     [_, -1, _, -5, 3, _],  # c
-#     [_, _, _, _, 5, 0],  # d
+#     [_, 1, _, -2, 5, 0],  # d
 #     [_, _, 2, 2, _, 9],  # e
 #     [_, _, _, _, _, _],  # f
 # ]
 
 
-print(dijkstra(graph, 0, 5))
+print(bellman_ford(graph_to_dict(graph), 'A', 'F'))
+print(bellman_ford_new(graph, 0, 5))
